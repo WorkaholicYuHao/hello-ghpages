3dcb0f4b052786333192d75eb4bc9edbca506507
于浩 <44229696+WorkaholicYuHao@users.noreply.github.com>
2018-11-12 11:07:15 +0800

    更新

--- src/main/js/select.js
+++ src/main/js/select.js
@@ -1,276 +1,275 @@
-(function(window){
+(function(window) {
 
-	var document = window.document;
+    var document = window.document;
 
-	var TPLS = {
-		selectboxHtml:
-			'<div class="QPUI-selectbox" hidefocus="true" tabindex="-1" style="{{style}}">'+
-				'<div class="QPUI-selectbox-inner" data-value="{{value}}">{{textContent}}</div>'+
-				'<i class="QPUI-selectbox-icon"></i>'+
-			'</div>',
+    var TPLS = {
+            selectboxHtml: '<div class="QPUI-selectbox" hidefocus="true" tabindex="-1" style="{{style}}">' +
+                '<div class="QPUI-selectbox-inner" data-value="{{value}}">{{textContent}}</div>' +
+                '<i class="QPUI-selectbox-icon"></i>' +
+                '</div>',
 
-		dropdownHtml: '<div class="QPUI-dropdown-wrap"><dl class="QPUI-selectbox-dropdown" role="menu">{{options}}</dl><div>',
-		optgroupHtml: '<dt class="QPUI-selectbox-optgroup">{{label}}</dt>',
-		optionHtml: '<dd class="QPUI-selectbox-option {{className}}"  data-value="{{value}}" data-option="{{index}}" tabindex="-1">{{textContent}}</dd>'
-	}
-	/**
-	 * »ñÈ¡ÔªËØÏà¶ÔÎ»ÖÃ
-	 * @param  {Object} field ÔªËØ
-	 * @return {Object}       °üº¬leftºÍtop
-	 */
-	function getPos(field, maxHeight){
-		var p = field.offset(),
-			scrollBarPos = $(window).height() + $(document).scrollTop();
-			topPos = p.top + field.outerHeight(),
-			leftPos = p.left;
-		topPos = (topPos + maxHeight) > scrollBarPos ? (p.top - maxHeight -2) : topPos;
-		return {
-			top: topPos,
-			left: leftPos
-		};
-	}
-	/**
-	 * Ä£°åÌæ»»
-	 * @param  {String} tpl  Ä£°å
-	 * @param  {Object} data Êý¾Ý
-	 * @return {String}      Æ´½ÓºóµÄÄ£°å
-	 */
-	function _tpl(tpl, data) {
-		return tpl.replace(/{{(.*?)}}/g, function ($1, $2) {
-			return data[$2];
-		});
-	}
-	/**
-	 * »ñÈ¡select option
-	 * @param  {Object} select selectÔªËØ
-	 * @param  {Number} index  Ë÷Òý
-	 * @return {Object}        Ë÷ÒýÔªËØ
-	 */
-	function _getOption(select, index) {
-		index = index === undefined ? select[0].selectedIndex : index;
-		return select.find('option').eq(index);
-	}
-
-	
-	/**
-	 * »ñÈ¡selectÐÅÏ¢
-	 * @param  {Object} select selectÔªËØ
-	 * @return {Object}        selectÐÅÏ¢
-	 */
-	function getSelectInfo(select){
-
-		var optionStr = '',
-			selectIndex = select[0].selectedIndex,
-			index = 0,
-			selectData = select.data()
+            dropdownHtml: '<div class="QPUI-dropdown-wrap"><dl class="QPUI-selectbox-dropdown" role="menu">{{options}}</dl><div>',
+            optgroupHtml: '<dt class="QPUI-selectbox-optgroup">{{label}}</dt>',
+            optionHtml: '<dd class="QPUI-selectbox-option {{className}}"  data-value="{{value}}" data-option="{{index}}" tabindex="-1">{{textContent}}</dd>'
+        }
+        /**
+         * ï¿½ï¿½È¡Ôªï¿½ï¿½ï¿½ï¿½ï¿½Î»ï¿½ï¿½
+         * @param  {Object} field Ôªï¿½ï¿½
+         * @return {Object}       ï¿½ï¿½ï¿½ï¿½leftï¿½ï¿½top
+         */
+    function getPos(field, maxHeight) {
+        var p = field.offset(),
+            scrollBarPos = $(window).height() + $(document).scrollTop();
+        topPos = p.top + field.outerHeight(),
+            leftPos = p.left;
+        topPos = (topPos + maxHeight) > scrollBarPos ? (p.top - maxHeight - 2) : topPos;
+        return {
+            top: topPos,
+            left: leftPos
+        };
+    }
+    /**
+     * Ä£ï¿½ï¿½ï¿½æ»»
+     * @param  {String} tpl  Ä£ï¿½ï¿½
+     * @param  {Object} data ï¿½ï¿½ï¿½ï¿½
+     * @return {String}      Æ´ï¿½Óºï¿½ï¿½Ä£ï¿½ï¿½
+     */
+    function _tpl(tpl, data) {
+        return tpl.replace(/{{(.*?)}}/g, function($1, $2) {
+            return data[$2];
+        });
+    }
+    /**
+     * ï¿½ï¿½È¡select option
+     * @param  {Object} select selectÔªï¿½ï¿½
+     * @param  {Number} index  ï¿½ï¿½ï¿½ï¿½
+     * @return {Object}        ï¿½ï¿½ï¿½ï¿½Ôªï¿½ï¿½
+     */
+    function _getOption(select, index) {
+        index = index === undefined ? select[0].selectedIndex : index;
+        return select.find('option').eq(index);
+    }
 
 
-		function getOptionsData(options){
-			options.each(function(){
-				var that = $(this);
-				var tplDate = $.extend({
-					className: 'drapdown',
-					index: index,
-					value: that.val(),
-					textContent: that.html()
-				}, that.data(), selectData);
+    /**
+     * ï¿½ï¿½È¡selectï¿½ï¿½Ï¢
+     * @param  {Object} select selectÔªï¿½ï¿½
+     * @return {Object}        selectï¿½ï¿½Ï¢
+     */
+    function getSelectInfo(select) {
 
-				optionStr += _tpl(TPLS.optionHtml, tplDate);
-				index++;
-			});
-		}
-
-		// Èç¹ûÓÐ·Ö×é Ö´ÐÐ·Ö×é
-		if (select.find('optgroup').length) {
-			select.find('optgroup').each(function (index) {
-				optionStr += _tpl(TPLS.optgroupHtml, {
-					index: index,
-					label: this.label
-				});
-				getOptionsData($(this).find('option'));
-			});	
-		}else{
-			getOptionsData(select.find('option'));
-		}
-		// »ñÈ¡Ñ¡ÖÐ
-		var selectedOptions = _getOption(select);
-		var selectboxHtml = _tpl(TPLS.selectboxHtml, {
-			style: select.attr('style') || '', 
-			textContent: selectedOptions.html() || '',
-			value: selectedOptions.val()
-		});
-
-		return {
-			'width': select.outerWidth(),
-			'height': select.outerHeight(),
-			'left': select.offset().left,
-			'top': select.offset().top,
-			'selectBox': selectboxHtml,
-			'dropdown': _tpl(TPLS.dropdownHtml, {
-				options: optionStr
-			})
-		}
-	}
-	/**
-	 * select Àà
-	 * @param {Object} select selectÔªËØ
-	 * @param {Object} opts   ÅäÖÃÏî
-	 */
-	var Select = function(select, opts){
-		var self = this;
-		$.extend(TPLS, opts);
-
-		self.select = select;
-
-		self.openClass = opts.openClass;
-		self.selectedClass = opts.selectedClass
-		self.onClose = opts.onClose;
-
-		self.isIE6 = !('minWidth' in select[0].style);
-
-		self.init();
-	}
-
-	Select.prototype = {
-		init: function(){
-			var self = this;
-			//
-			var selectInfo = self.selectInfo = getSelectInfo(self.select);
-			self.select.hide();
-
-			self._selectBox = $(selectInfo.selectBox);
-			self._value = self._selectBox.find('[data-value]');
+        var optionStr = '',
+            selectIndex = select[0].selectedIndex,
+            index = 0,
+            selectData = select.data()
 
 
-			self._selectBox.css({
-				width: selectInfo.width
-			});
+        function getOptionsData(options) {
+            options.each(function() {
+                var that = $(this);
+                var tplDate = $.extend({
+                    className: 'drapdown',
+                    index: index,
+                    value: that.val(),
+                    textContent: that.html()
+                }, that.data(), selectData);
 
-			self.select.after(self._selectBox);
-			self.bind();
-		},
+                optionStr += _tpl(TPLS.optionHtml, tplDate);
+                index++;
+            });
+        }
 
-		show: function(){
-			var self = this,
-				select = self.select,
-				selectbox = self._selectBox,
-				selectInfo = self.selectInfo;
+        // ï¿½ï¿½ï¿½ï¿½Ð·ï¿½ï¿½ï¿½ Ö´ï¿½Ð·ï¿½ï¿½ï¿½
+        if (select.find('optgroup').length) {
+            select.find('optgroup').each(function(index) {
+                optionStr += _tpl(TPLS.optgroupHtml, {
+                    index: index,
+                    label: this.label
+                });
+                getOptionsData($(this).find('option'));
+            });
+        } else {
+            getOptionsData(select.find('option'));
+        }
+        // ï¿½ï¿½È¡Ñ¡ï¿½ï¿½
+        var selectedOptions = _getOption(select);
+        var selectboxHtml = _tpl(TPLS.selectboxHtml, {
+            style: select.attr('style') || '',
+            textContent: selectedOptions.html() || '',
+            value: selectedOptions.val()
+        });
 
-			if (select[0].disabled || !select[0].length) {
-				return false;
-			};
+        return {
+            'width': select.outerWidth(),
+            'height': select.outerHeight(),
+            'left': select.offset().left,
+            'top': select.offset().top,
+            'selectBox': selectboxHtml,
+            'dropdown': _tpl(TPLS.dropdownHtml, {
+                options: optionStr
+            })
+        }
+    }
+    /**
+     * select ï¿½ï¿½
+     * @param {Object} select selectÔªï¿½ï¿½
+     * @param {Object} opts   ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+     */
+    var Select = function(select, opts) {
+        var self = this;
+        $.extend(TPLS, opts);
 
-			var ie6 = self.isIE6;
+        self.select = select;
 
-			var selectHeight = selectInfo.height;
-			var topHeight = selectInfo.top - $(document).scrollTop();
-			var bottomHeight = $(window).height() - topHeight - selectHeight;
-			var maxHeight = Math.max(topHeight, bottomHeight) - 20;
+        self.openClass = opts.openClass;
+        self.selectedClass = opts.selectedClass
+        self.onClose = opts.onClose;
 
-			var _dropdown = self._dropdown = $(selectInfo.dropdown);
-			var selectBoxPos = getPos(selectbox, maxHeight);
-			_dropdown.css({
-				'position': 'absolute',
-				'top': selectBoxPos.top,
-				'left': selectBoxPos.left
-			});
+        self.isIE6 = !('minWidth' in select[0].style);
 
-			var children = _dropdown.children()
-			children.css({
-				minWidth: selectbox.innerWidth(),
-				maxHeight: maxHeight,
-				width: ie6 ? Math.max(selectbox.innerWidth(), children.outerWidth()) : 'auto',
-				height: ie6 ? Math.min(maxHeight, children.outerHeight()) : 'auto',
-				overflowY: 'auto',
-				overflowX: 'hidden'
-			});
+        self.init();
+    }
 
-			var selectIndex = select[0].selectedIndex;
-			children.find('[data-option]').eq(selectIndex).addClass(self.selectedClass);
+    Select.prototype = {
+        init: function() {
+            var self = this;
+            //
+            var selectInfo = self.selectInfo = getSelectInfo(self.select);
+            self.select.hide();
 
-			selectbox.addClass(self.openClass);
-
-			$('body').append(_dropdown);
+            self._selectBox = $(selectInfo.selectBox);
+            self._value = self._selectBox.find('[data-value]');
 
 
-			self._dropdown.delegate('[data-option]', 'click', function(){
-				var index = $(this).attr('data-option');
-				self.selected(index);
-			});
-		},
-		close: function(){
-			var self = this;			
-			self._selectBox.removeClass(self.openClass);
-			self.onClose && self.onClose.call(self);
-			if(!self._dropdown){
-				return
-			}
-			self._dropdown.undelegate();
-			self._dropdown.remove();
-			self._dropdown = null;
-		},
-		selected: function(index){
-			var self = this;
+            self._selectBox.css({
+                width: selectInfo.width
+            });
 
-			var st = _getOption(self.select, index);
+            self.select.after(self._selectBox);
+            self.bind();
+        },
 
-			if (st.attr('disabled')) {
-				return false;
-			}
+        show: function() {
+            var self = this,
+                select = self.select,
+                selectbox = self._selectBox,
+                selectInfo = self.selectInfo;
 
-			self.select[0].selectedIndex = index;
+            if (select[0].disabled || !select[0].length) {
+                return false;
+            };
 
-			self._value.html(st.html());
+            var ie6 = self.isIE6;
 
-			self.close();
+            var selectHeight = selectInfo.height;
+            var topHeight = selectInfo.top - $(document).scrollTop();
+            var bottomHeight = $(window).height() - topHeight - selectHeight;
+            var maxHeight = Math.max(topHeight, bottomHeight) - 20;
 
-		},
-		bind: function(){
-			var self = this;
-			self._selectBox.on('click', function(){
-				self.click();
-			});
+            var _dropdown = self._dropdown = $(selectInfo.dropdown);
+            var selectBoxPos = getPos(selectbox, maxHeight);
+            _dropdown.css({
+                'position': 'absolute',
+                'top': selectBoxPos.top,
+                'left': selectBoxPos.left
+            });
 
-			// µã»÷¿Õ°×´¦¹Ø±Õ
-			$(document).on('click', function(ev){
-				var target = ev.target || ev.srcElement;
-				
-				if (!target) {
-					return;
-				};
+            var children = _dropdown.children()
+            children.css({
+                minWidth: selectbox.innerWidth(),
+                maxHeight: maxHeight,
+                width: ie6 ? Math.max(selectbox.innerWidth(), children.outerWidth()) : 'auto',
+                height: ie6 ? Math.min(maxHeight, children.outerHeight()) : 'auto',
+                overflowY: 'auto',
+                overflowX: 'hidden'
+            });
 
-				do{
-					if(target == self._selectBox[0] || !target || 
-						(self._dropdown && target == self._dropdown[0])){
-						return;
-					}
-				}
-				while(target = target.parentNode);
+            var selectIndex = select[0].selectedIndex;
+            children.find('[data-option]').eq(selectIndex).addClass(self.selectedClass);
 
-				self.close();
+            selectbox.addClass(self.openClass);
 
-			})
-		},
-		click: function(){
-			var self = this;
-			if (!self.select[0].disabled) {
-				self._dropdown ? self.close() : self.show();
-			};
-		}
+            $('body').append(_dropdown);
 
-	}
 
-	// Ä¬ÈÏ²ÎÊý
-	var DEFAULTCONFIG = {
-		openClass: 'QPUI-selectbox-open',
-		selectedClass: 'QPUI-selectbox-selected',
-		onClose: function(){}
-	}
+            self._dropdown.delegate('[data-option]', 'click', function() {
+                var index = $(this).attr('data-option');
+                self.selected(index);
+            });
+        },
+        close: function() {
+            var self = this;
+            self._selectBox.removeClass(self.openClass);
+            self.onClose && self.onClose.call(self);
+            if (!self._dropdown) {
+                return
+            }
+            self._dropdown.undelegate();
+            self._dropdown.remove();
+            self._dropdown = null;
+        },
+        selected: function(index) {
+            var self = this;
 
-	$.fn.selectbox = function(opts){
-		opts = $.extend({}, DEFAULTCONFIG, opts);
-		return this.each(function(){
-			new Select($(this), opts);
-		});
-	}
+            var st = _getOption(self.select, index);
+
+            if (st.attr('disabled')) {
+                return false;
+            }
+
+            self.select[0].selectedIndex = index;
+
+            self._value.html(st.html());
+
+            self.close();
+
+        },
+        bind: function() {
+            var self = this;
+            self._selectBox.on('click', function() {
+                self.click();
+            });
+
+            // ï¿½ï¿½ï¿½ï¿½Õ°×´ï¿½ï¿½Ø±ï¿½
+            $(document).on('click', function(ev) {
+                var target = ev.target || ev.srcElement;
+
+                if (!target) {
+                    return;
+                };
+
+                do {
+                    if (target == self._selectBox[0] || !target ||
+                        (self._dropdown && target == self._dropdown[0])) {
+                        return;
+                    }
+                }
+                while (target = target.parentNode);
+
+                self.close();
+
+            })
+        },
+        click: function() {
+            var self = this;
+            if (!self.select[0].disabled) {
+                self._dropdown ? self.close() : self.show();
+            };
+        }
+
+    }
+
+    // Ä¬ï¿½Ï²ï¿½ï¿½ï¿½
+    var DEFAULTCONFIG = {
+        openClass: 'QPUI-selectbox-open',
+        selectedClass: 'QPUI-selectbox-selected',
+        onClose: function() {}
+    }
+
+    $.fn.selectbox = function(opts) {
+        opts = $.extend({}, DEFAULTCONFIG, opts);
+        return this.each(function() {
+            new Select($(this), opts);
+        });
+    }
 })(this);
